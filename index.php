7a059ddad499714624ee0f5d785c01985fafaa22
Lampadaire <julienrau1999@gmail.com>
2019-05-25 22:25:06 +0200

    Ajout des infos

--- index.php
+++ index.php
@@ -18,13 +18,24 @@
     <?php require_once('header.php'); ?>
 
 		<!-- Contenu principale -->
-		<section class="container text-center mt-5 text-white principale">
-			<div class="card text-center bg-dark">
-				<div class="card-header">
+		<section class="container mt-5 text-white principale">
+			<div class="card bg-dark">
+				<div class="card-header text-center">
 					<h2>Accueil</h2>
 				</div>
 				<div class="card-body">
-					<p>Bienvenue sur QUIZUP</p>
+					<h4 class="text-center">Bienvenue sur QUIZUP !</h4>
+					
+					<br>
+					<p>Site crée par : </p>
+
+					<ol>
+						<li>Thomas Beck -> Sprint Bleu</li>
+						<li>Ahn Arnaud -> Sprint Rose</li>
+						<li>Rausin Julien -> Sprint Vert</li>
+					</ol>
+
+					<p>Sprint orange réalisé par Thomas Beck, Ahn Arnaud et Rausin Julien !</p>
 				</div>
 				<div class="card-footer">
 					
